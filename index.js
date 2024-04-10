64a6b0a56978c0bca1bda31d3fb05ef25b8e570e
Allan de Brito Neves <allanbritoneves@hotmail.com>
2024-04-10 08:33:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,60 @@
-const nodemailer = require("nodemailer");
-
 // Adicione um evento para aguardar o carregamento completo da página
-document.addEventListener("DOMContentLoaded", function () {
-  reload();
-});
-
-// Função para carregar o envio de email após o carregamento da página
-function reload() {
+window.onload = function() {
   // Obtém o formulário e o elemento de mensagem
-  const form = document.getElementById("newsletterForm");
-  const mensagemElemento = document.getElementById("mensagem");
+  const form = document.getElementById('newsletterForm');
+  const mensagemElemento = document.getElementById('mensagem');
 
   // Adiciona um ouvinte de evento para o envio do formulário
-  form.addEventListener("submit", function (event) {
-    // Impede o envio padrão do formulário
-    event.preventDefault();
+  form.addEventListener('submit', function(event) {
+      // Impede o envio padrão do formulário
+      event.preventDefault();
 
-    // Obtém o valor do email do campo de entrada
-    const email = document.getElementById("emailInput").value;
+      // Obtém o valor do email do campo de entrada
+      const email = document.getElementById('emailInput').value;
 
-    // Valida o email
-    if (validarEmail(email)) {
-      // Envie o email com nodemailer
-      enviarEmail(email);
-    } else {
-      mensagemElemento.textContent = "Por favor, insira um e-mail válido";
-    }
+      // Valida o email
+      if (validarEmail(email)) {
+          // Envie o email com nodemailer
+          enviarEmail(email);
+      } else {
+          mensagemElemento.textContent = 'Por favor, insira um e-mail válido';
+      }
   });
-}
-
-// Função para enviar o email
-function enviarEmail(email) {
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "allanbritoneves@gmail.com",
-      pass: "xjulfwbfirllffjo",
-    },
-  });
-
-  let options = {
-    from: "Seu Nome <seuemail@gmail.com>",
-    to: email,
-    subject: "Meu primeiro email",
-    text: "Bom Dia!",
-    //html: mensagem
-  };
-
-  const sendMail = async () => {
-    try {
-      console.log("enviando email");
-      await transporter.sendMail(options);
-      console.log("email enviado!");
-    } catch (error) {
-      console.log("deu erro!");
-    }
-  };
-
-  // Chama a função sendMail para enviar o email
-  sendMail();
-}
+};
 
 // Função para validar o email usando uma expressão regular simples
 function validarEmail(email) {
   const re = /\S+@\S+\.\S+/;
   return re.test(email);
+}
+
+// Função para enviar email com Nodemailer
+function enviarEmail(email) {
+  const nodemailer = require('nodemailer');
+
+  const transport = nodemailer.createTransport({
+      host: 'smtp.mailersend.net',
+      port: '587',
+      secure: false,
+      auth: {
+          user: 'MS_gjeWkR@trial-yzkq340k85kgd796.mlsender.net',
+          pass: 'JsMrV2ED4Jfia3ig',
+      }
+  });
+
+  transport.sendMail({
+      from: 'Seja Bem Vindo <allanbritoneves@hotmail.com>',
+      to: email,
+      subject: 'Enviando email com nodemailer',
+      html: '<h1>Olá, Seja bem Vindo a nossa Newsletter',
+      text: 'Olá, Seja bem Vindo a nossa Newsletter'
+  })
+  .then(() => {
+      console.log('Email enviado com sucesso!');
+      mensagemElemento.textContent = 'Obrigado por se inscrever na nossa newsletter!';
+  })
+  .catch((error) => {
+      console.error('Erro ao enviar email', error);
+      mensagemElemento.textContent = 'Ocorreu um erro ao tentar enviar o email. Por favor, tente novamente mais tarde.';
+  });
 }
